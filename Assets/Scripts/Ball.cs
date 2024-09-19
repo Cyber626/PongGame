a4e27f8f68555134386c0a1ec0895c5939221e06
Cyber626 <xakimovbexzodjon@gmail.com>
2024-09-19 17:49:28 +0300

    GameManager

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,16 +7,40 @@ public class Ball : MonoBehaviour
 {
 
     [SerializeField] private float xForce, yForce;
-    [SerializeField] private GameObject obstacle;
+    [SerializeField] private GameManager manager;
     private Rigidbody2D rb;
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
     private void Start()
     {
+        StartCoroutine(StartFromCenter());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "PlayerTrigger")
+        {
+            manager.EnemyScored();
+        }
+        else if (collision.tag == "EnemyTrigger")
+        {
+            manager.PlayerScored();
+        }
+        StartCoroutine(StartFromCenter());
+    }
+
+    private IEnumerator StartFromCenter()
+    {
+        transform.position = Vector3.zero;
+        rb.velocity = Vector3.zero;
+
         float xDir = UnityEngine.Random.value - .5f;
         float yDir = UnityEngine.Random.value - .5f;
+
+        yield return new WaitForSeconds(.5f);
 
         rb.AddForce(new Vector2(xForce * Mathf.Sign(xDir), yForce * Mathf.Sign(yDir)));
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 
+    [SerializeField] private TextMeshProUGUI playerScoreText, enemyScoreText;
+
     public int playerScore { get; private set; }
     public int enemyScore { get; private set; }
+
+    private void Start()
+    {
+        playerScore = 0;
+        enemyScore = 0;
+        playerScoreText.text = playerScore.ToString();
+        enemyScoreText.text = enemyScore.ToString();
+    }
+
+    public void EnemyScored()
+    {
+        enemyScore++;
+        enemyScoreText.text = enemyScore.ToString();
+    }
+
+    public void PlayerScored()
+    {
+        playerScore++;
+        playerScoreText.text = playerScore.ToString();
+    }
 
 }
